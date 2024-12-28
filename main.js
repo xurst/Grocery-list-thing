f6fd0264f52491f1b0b9221ea5160f2fe1de366c
xurst <139590166+xurst@users.noreply.github.com>
2024-12-27 22:08:20 -0500

    better security

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // main.js
-const CLIENT_ID = '855325079958-hl4gqsg23q6sk0nbu4sunpun5d3ltkp1.apps.googleusercontent.com';
-const API_KEY = 'AIzaSyBhsojdbofAzq_v3dxZrgqfj4521zpuqRE';
-const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
-const SCOPES = 'https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.file';
-const DOC_ID = '11NS1eX7fUaSEqOJpTsfWBOVaYfpWEP32ITCJy9HDUqI';
+const CLIENT_ID = CONFIG.CLIENT_ID;
+const API_KEY = CONFIG.API_KEY;
+const DISCOVERY_DOC = CONFIG.DISCOVERY_DOC;
+const SCOPES = CONFIG.SCOPES;
+const DOC_ID = CONFIG.DOC_ID;
 
 let tokenClient;
 let gapiInited = false;
