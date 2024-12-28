5aecf23c6aa0ba1d14adf50fad02d1350de52340
xurst <139590166+xurst@users.noreply.github.com>
2024-12-27 22:10:14 -0500

    Update .gitignore and remove sensitive files

--- config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-    CLIENT_ID: '855325079958-hl4gqsg23q6sk0nbu4sunpun5d3ltkp1.apps.googleusercontent.com',
-    API_KEY: 'AIzaSyBhsojdbofAzq_v3dxZrgqfj4521zpuqRE',
-    DISCOVERY_DOC: 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest',
-    SCOPES: 'https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.file',
-    DOC_ID: '11NS1eX7fUaSEqOJpTsfWBOVaYfpWEP32ITCJy9HDUqI',
-};
